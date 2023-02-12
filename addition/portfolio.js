9d392af08ba9600619433ec9c653726c2369d7ca
AdhikariDarpan <124805232+AdhikariDarpan@users.noreply.github.com>
2023-02-12 07:12:47 +0545

    Update portfolio.js

--- addition/portfolio.js
+++ addition/portfolio.js
@@ -338,8 +338,9 @@ darkModeBtn.addEventListener("click", (e) => {
       hr = clockDate.getHours();
       min = clockDate.getMinutes();
       sec = clockDate.getSeconds();
-      if(hr < 10) {min = "0"+hr;}
+      if(hr < 10) {hr = "0"+hr;}
       if(min < 10) {min = "0"+min;}
+      if(sec < 10) {sec = "0"+sec;}
     
       hours.textContent = hr;
       minutes.textContent = min;
