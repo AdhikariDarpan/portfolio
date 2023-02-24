91be43a6be57f1c6d80484f317b0f75aeddbfed2
AdhikariDarpan <124805232+AdhikariDarpan@users.noreply.github.com>
2023-02-25 00:05:16 +0545

    snow fall

--- addition/portfolio.js
+++ addition/portfolio.js
@@ -357,3 +357,45 @@ darkModeBtn.addEventListener("click", (e) => {
       minutes.textContent = min;
       second.textContent = sec;
     }); 
+
+// background snow fall---------------------------------------------------------
+const cnv = document.querySelector("#canvas");
+const ctx = cnv.getContext("2d");
+cnv.width = window.innerWidth;
+cnv.height = window.innerHeight;
+let dropsCountX,dropsCountY;
+class Drop{
+  constructor(){
+    this.x = Math.random() * cnv.width;
+    this.y = Math.random() * cnv.height;
+    this.r = Math.random() * 2.5;
+    this.v = Math.random() * 3.5;
+    
+  }
+  make(){
+    ctx.fillStyle = "#fff";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+    this.y += this.v;
+
+    if(this.y > cnv.height){
+      this.x = Math.random() * cnv.width;
+      this.y = 0;
+    }
+  }
+}
+let drops = [];
+for(let i=0;i<1000;i++) drops.push(new Drop());
+function anm(){
+  ctx.fillStyle = "rgba(0,0,0,0.5)";
+  ctx.fillRect(0,0,cnv.width, cnv.height);
+  drops.forEach(drop => drop.make());
+  requestAnimationFrame(anm);
+}
+anm();
+window.addEventListener("resize",()=>{
+  cnv.width = window.innerWidth;
+  cnv.height = window.innerHeight;
+});
